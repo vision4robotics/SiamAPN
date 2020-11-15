23e98529c4d591f14cef3938ec5b45759e8790aa
vision4robotics <fuchanghong_008@163.com>
2020-11-15 22:12:35 +0800

    update

--- toolkit/datasets/__init__.py
+++ toolkit/datasets/__init__.py
@@ -1,25 +1,11 @@
-from .otb import OTBDataset
-from .uav import UAVDataset
-from .lasot import LaSOTDataset
-from .got10k import GOT10kDataset
-from .dtb import DTBDataset
-from .uavdt import UAVDTDataset
 from .uav10fps import UAV10Dataset
 from .uav20l import UAV20Dataset
-from .visdrone import VISDRONEDDataset
 from .visdrone1 import VISDRONED2018Dataset
 class DatasetFactory(object):
     @staticmethod
     def create_dataset(**kwargs):
         """
-        Args:
-            name: dataset name 'OTB2015', 'LaSOT', 'UAV123', 'NFS240', 'NFS30',
-                'VOT2018', 'VOT2016', 'VOT2018-LT'
-            dataset_root: dataset root
-            load_img: wether to load image
-        Return:
-            dataset
-        """
+
         assert 'name' in kwargs, "should provide dataset name"
         name = kwargs['name']
         if 'UAV10fps' in name:
